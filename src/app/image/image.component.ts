bbf465770ddbef492dea041a3ba11c57f18f1c49
just2click <just2click@outlook.com>
2017-11-11 15:22:33 +0200

    Initial commit

---
+++ src/app/image/image.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
